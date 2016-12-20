905f7ef931c0a9133f61b1695969f5c279d04d1c
yasodha <yasodha.bitra@gmail.com>
2016-12-20 13:47:27 -0800

    clientview class missing

---
+++ Analytics/Models/ClientView.cs
@@ -0,0 +1,18 @@
+﻿using Analytics.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Analytics.Models
+{
+    public class ClientView:BaseEntity
+    {
+        public int id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool? IsActive { get; set; }
+       // public List<ClientView> ClientsList { get; set; }
+    }
+}
